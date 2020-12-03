36acfb82fb377ee558b1990a8d3987be455aeb7a
Azmah-Bad <56384031+Azmah-Bad@users.noreply.github.com>
2020-12-03 14:32:50 +0100

    👋 init commit of hard decoder
    Signed-off-by: Azmah-Bad <56384031+Azmah-Bad@users.noreply.github.com>

---
+++ Hard_decoder/hard_decoder.m
@@ -0,0 +1,12 @@
+% Author HBA CLA
+
+
+function c_cor = hard_decoder(c, H, MAX_ITER)
+    % c matrice de taille (N,1)
+    % M matrice binaire de taille (M,N)
+    % MAX_ITER int 
+
+    [c_rows, c_cols] = size(c);
+    [H_rows, H_cols] = size(H);
+
+end
