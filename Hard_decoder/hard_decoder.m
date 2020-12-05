4f6d9284ee690519465fc62c93eba69e60e21c78
clementlagneau <clement-lagneau@insa-lyon.fr>
2020-12-05 21:31:32 +0100

    Hard decoder steps 2-3-4

--- Hard_decoder/hard_decoder.m
+++ Hard_decoder/hard_decoder.m
@@ -2,12 +2,12 @@
 
 
 function c_cor = hard_decoder(c, H, MAX_ITER)
-    % c matrice de taille (N,1)
-    % M matrice binaire de taille (M,N)
-    % MAX_ITER int 
+    % c matrice de taille (N,1) - Flux de bits 
+    % M matrice binaire de taille (M,N) - Matrice d association 
+    % MAX_ITER int - Maximum d iteration
 
-    [c_rows, c_cols] = size(c);
-    [H_rows, H_cols] = size(H);
+    [c_rows, c_cols] = size(c); %On recupere le nombre de colonne et le nombre de ligne de c 
+    [H_rows, H_cols] = size(H); %On recupere le nombre de colonne et le nombre de ligne de H
 
     % TODO :: check input confomity 
 
@@ -22,6 +22,8 @@ function c_cor = hard_decoder(c, H, MAX_ITER)
         Messages = -1 * ones(number_of_Cnodes,number_of_Vnodes);
         Responses = -1 * ones(number_of_Cnodes,number_of_Vnodes);
 
+        %First step : each node send the bit their received
+        %Craft message to send from c_i to f_j
         for ligne = 1:H_rows
             for colonne = 1:H_cols
                 if H(ligne, colonne) == 1
@@ -30,10 +32,11 @@ function c_cor = hard_decoder(c, H, MAX_ITER)
             end
         end
 
+        %Second step : each check node calculate and respond
         % Response block 
         for ligne = 1:number_of_Vnodes
             for colonne = 1:number_of_Cnodes
-                if Messages(ligne, colonne) ~= 1
+                if Messages(ligne, colonne) ~= -1
                     parity_total = 0;
                     for total_colomn_parity = 1:number_of_Vnodes
                         if (total_colomn_parity ~= colonne) && (Messages(ligne,total_colomn_parity) ~= -1)
@@ -48,19 +51,60 @@ function c_cor = hard_decoder(c, H, MAX_ITER)
                 end
             end
         end
-end
+        
+        %check if algorithm terminates
+        is_vector_even = zeros(number_of_Cnodes, 1);
+        for ligne = 1:number_of_Cnodes
+            parity = 0;
+            for colonne = 1:number_of_Vnodes
+                if H(ligne, colonne) ~= 0
+                    parity = parity + c(colonne);
+                end
+            end
+            is_vector_even(ligne) = mod(parity, 2);
+        end
+        if isequal(Messages, Responses)
+            %Algorithm ends
+            c_cor = iteration_c;
+            return
+        end
 
-
-function is_vector_even = parity_check(c, H, c_nodes, v_nodes)
-    % Do the parity check for the given c
-    is_vector_even = zeros(c_nodes, 1);
-    for row = 1:c_nodes
-        parity = 0;
-        for col = 1:v_nodes
-            if H(row, col) ~= 0
-                parity = parity + c(col);
+        %Thid step : use additional information
+        %Majority vote
+        majority_vote = ones(number_of_Vnodes,number_of_Cnodes);
+        majority_index = 1
+        for colonne = 1:number_of_Vnodes
+            for c_node_index = 1:number_of_Vnodes
+                if Responses(c_node_index, colonne) ~= -1
+                    majority_vote(colonne, majority_index) = responses(c_node_index, colonne);
+                    majority_index = majority_index + 1;
+                end
             end
         end
-        is_vector_even(row) = mod(parity, 2);
+
+        for ligne = 1:number_of_Vnodes
+            [most_frequent, frequency] = mode(majority_vote(ligne, :));
+            if (mod(number_of_Cnodes+1, 2)) == 0 && (frequency == (number_of_Cnodes+1/2))
+                iteration_c(ligne, 1) = randi([0, 1], [1, 1]);
+            else
+                iteration_c(ligne, 1) = most_frequent;
+            end
+        end
+
+        is_vector_even = zeros(number_of_Cnodes, 1);
+        for ligne = 1:number_of_Cnodes
+            parity = 0;
+            for colonne = 1:number_of_Vnodes
+                if H(ligne, colonne) ~= 0
+                    parity = parity + c(colonne);
+                end
+            end
+            is_vector_even(ligne) = mod(parity, 2);
+        end
+        if sum(is_vector_even) == 0
+            c_cor = iteration_c;
+            return
+        end
     end
+    c_cor = iter_c;
 end
