82cbdec0bef3223bbea4e299dfe98c55fd96d9d8
clementlagneau <clement-lagneau@insa-lyon.fr>
2020-12-12 17:55:26 +0100

    Up

--- Hard_decoder/HARD_DECODER_GROUPE2.m
+++ Hard_decoder/HARD_DECODER_GROUPE2.m
@@ -11,9 +11,12 @@ function c_cor = HARD_DECODER_GROUPE2(c, H, MAX_ITER)
 
     % TODO :: check input confomity 
 
-    number_of_Vnodes = c_cols;
-    number_of_Cnodes = c_rows;
+    number_of_Vnodes = c_rows;
+    number_of_Cnodes = H_rows;
 
+    majority_vote = NaN(number_of_Vnodes, number_of_Cnodes);
+    majority_vote(:,1) = c;
+    
     iteration_c = c;
 
     % main loop 
@@ -34,8 +37,8 @@ function c_cor = HARD_DECODER_GROUPE2(c, H, MAX_ITER)
 
         %Second step : each check node calculate and respond
         % Response block 
-        for ligne = 1:number_of_Vnodes
-            for colonne = 1:number_of_Cnodes
+        for ligne = 1:number_of_Cnodes
+            for colonne = 1:number_of_Vnodes
                 if Messages(ligne, colonne) ~= -1
                     parity_total = 0;
                     for total_colomn_parity = 1:number_of_Vnodes
@@ -54,9 +57,9 @@ function c_cor = HARD_DECODER_GROUPE2(c, H, MAX_ITER)
         
         %check if algorithm terminates
         is_vector_even = zeros(number_of_Cnodes, 1);
-        for ligne = 1:number_of_Vnodes
+        for ligne = 1:number_of_Cnodes
             parity = 0;
-            for colonne = 1:number_of_Cnodes
+            for colonne = 1:number_of_Vnodes
                 if H(ligne, colonne) ~= 0
                     parity = parity + c(colonne);
                 end
@@ -71,18 +74,18 @@ function c_cor = HARD_DECODER_GROUPE2(c, H, MAX_ITER)
 
         %Thid step : use additional information
         %Majority vote
-        majority_vote = ones(number_of_Vnodes,number_of_Cnodes);
-        majority_index = 1;
+        %majority_vote = ones(number_of_Cnodes,number_of_Vnodes);
+        majority_index = 2;
         for colonne = 1:number_of_Vnodes
-            for c_node_index = 1:number_of_Vnodes
-                if Responses(c_node_index, colonne) ~= -1
-                    majority_vote(colonne, majority_index) = Responses(c_node_index, colonne);
+            for ligne = 1:number_of_Cnodes
+                if Responses(ligne, colonne) ~= -1
+                    majority_vote(colonne, majority_index) = Responses(ligne, colonne);
                     majority_index = majority_index + 1;
                 end
             end
         end
 
-        for ligne = 1:number_of_Vnodes
+        for ligne = 1:number_of_Cnodes
             [most_frequent, frequency] = mode(majority_vote(ligne, :));
             if (mod(number_of_Cnodes+1, 2)) == 0 && (frequency == (number_of_Cnodes+1/2))
                 iteration_c(ligne, 1) = randi([0, 1], [1, 1]);
@@ -92,9 +95,9 @@ function c_cor = HARD_DECODER_GROUPE2(c, H, MAX_ITER)
         end
 
         is_vector_even = zeros(number_of_Cnodes, 1);
-        for ligne = 1:number_of_Vnodes
+        for ligne = 1:number_of_Cnodes
             parity = 0;
-            for colonne = 1:number_of_Cnodes
+            for colonne = 1:number_of_Vnodes
                 if H(ligne, colonne) ~= 0
                     parity = parity + c(colonne);
                 end
